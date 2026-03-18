3f47921b78874ccaa1e47e11f97a2b11bb1e3181
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-18 07:59:19 +0530

    [TASK] : Integration with convex completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { api } from "@/convex/_generated/api";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { user } = useUser();
+  const createUser = useMutation(api.user.createUser);
+
+  useEffect(() => {
+    user && CheckUser();
+  }, [user]);
+
+  const CheckUser = async () => {
+    const result = await createUser({
+      email: user?.primaryEmailAddress?.emailAddress || "",
+      userName: user?.fullName || "",
+      imageUrl: user?.imageUrl || "",
+    });
+    console.log(result);
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Hello World</h1>
--- convex/schema.ts
+++ convex/schema.ts
@@ -4,7 +4,7 @@ import { v } from "convex/values";
 export default defineSchema({
   users: defineTable({
     email: v.string(),
-    useName: v.string(),
+    userName: v.string(),
     imageUrl: v.string()
   })
 })
