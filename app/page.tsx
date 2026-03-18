4ce1590d0e3925e6e89fe41007b0e5ad42e01978
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-18 07:32:47 +0530

    [TASK] : Authentication setup  with clerk completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Hello World</h1>
       <Button>Button</Button>
+      <UserButton />
     </div>
   );
 }
